348c1b86f253c9355ed5e2c92ae165e23ab45e69
first19326 <870390039@qq.com>
2020-09-15 20:32:27 +0800

    fixed search view questions

--- static/js/search.js
+++ static/js/search.js
@@ -1,25 +1,30 @@
 $(document).ready(function () {
-    var searchSettings = window.config.Search;
-    var isSearchSettingsValid = searchSettings.applicationID &&
+    let searchSettings = window.config.Search;
+    let isSearchSettingsValid = searchSettings.applicationID &&
                                 searchSettings.apiKey &&
                                 searchSettings.indexName;
-
+    let scroll = null;
     if (!isSearchSettingsValid) {
         window.console.error('Search settings are invalid.');
         return;
     }
 
-    var search = instantsearch({
+    let search = instantsearch({
         appId          : searchSettings.applicationID,
         apiKey         : searchSettings.apiKey,
         indexName      : searchSettings.indexName,
         searchFunction : function (helper) {
-            var searchInput = $('#search-input').find('input');
+            let searchInput = $('#search-input').find('input');
 
             if (searchInput.val()) {
                 helper.search();
             }
         }
+    });
+
+    // Search Render Function
+    search.on('render', function() {
+        scroll.refresh();
     });
 
     // Registering Widgets
@@ -34,7 +39,7 @@ $(document).ready(function () {
             hitsPerPage : searchSettings.hits.page || 10,
             templates   : {
                 item: function (data) {
-                    var link = data.permalink ? data.permalink : ('/' + data.path);
+                    let link = data.permalink ? data.permalink : ('/' + data.path);
                     return ('<a href="' + link + '" class="search-hit-link">' + data._highlightResult.title.value + '</a>');
                 },
                 empty: function (data) {
@@ -50,7 +55,7 @@ $(document).ready(function () {
             container : '#search-stats',
             templates : {
                 body: function (data) {
-                    var stats = searchSettings.labels.stats.replace(/\$\{hits}/, data.nbHits).replace(/\$\{time}/, data.processingTimeMS);
+                    let stats = searchSettings.labels.stats.replace(/\$\{hits}/, data.nbHits).replace(/\$\{time}/, data.processingTimeMS);
                     return (stats + '<hr/>');
                 }
             }
@@ -79,19 +84,44 @@ $(document).ready(function () {
         // 打开搜索窗口时, 关闭菜单
         $('.menu-button-close').click();
         $('body').append('<div class="search-cover"></div>').css('overflow', 'hidden');
+        // 移动端防止滚动
+        $('.search-cover').on("touchmove", function(event){
+            event.preventDefault;
+        }, false);
+        // 移动端防止滚动-输出框获取焦点时
+        document.body.addEventListener('touchmove', handler, { passive: false });
         $('.search-window').toggle();
         $('#search-input').find('input').focus();
+        // 初始化滚动条
+        let height = $(".search-content").outerHeight();
+        $(".search-scroll").css("height", "calc(100% - " + height + "px)");
+        scroll = new IScroll(".search-scroll", {
+            scrollbars: true,
+            mouseWheel: true,
+            fadeScrollbars: true,
+            resizePolling: 60
+        });
     });
 
     $('body').on('click', '.search-close-icon', function() {
         $('.search-window').hide();
         $('.search-cover').remove();
-        $('body').css('overflow', '');
+        // 移除移动端防止滚动监听-输出框获取焦点时
+        document.body.removeEventListener('touchmove', handler, { passive: false });
+        $('body').css('overflow', 'auto');
         // 关闭搜索窗口时, 搜索内容清空
         $('#search-input').find('input').val('');
         $('#search-stats').empty();
         $('#search-hits').empty();
         $('#search-pagination').empty();
+        // 销毁滚动条
+        scroll.destroy();
+        scroll = null;
     });
 
 });
+
+function handler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+}
